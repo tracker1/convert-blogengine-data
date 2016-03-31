8e4a2dd8c4c2fd30a88a23269c5265a0df1f2a99
Michael J. Ryan <tracker1@gmail.com>
2016-03-30 21:20:24 -0700

    post conversion is working

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-import getCategories from './lib/categories';
+import path from 'path';
+import fs from 'mz/fs';
+import getPostList from './lib/post-list';
+import readPost from './lib/post-read';
+import writePost from './lib/post-write';
+import getLegacyLinks from './lib/post-legacy-links';
 
 async function main() {
-  await getCategories();
+  try {
+    //var cats = await getCategories();
+    var cats = [];
+    var tags = [];
+    var maplinks = [];
+    var posts = await getPostList();
+    for (var p of posts) {
+      var post = await readPost(p);
+      tags = unique(tags.concat(post.tags));
+      cats = unique(cats.concat(post.categories));
+      maplinks = maplinks.concat(await getLegacyLinks(post));
+      await writePost(post);
+    }
+    
+    //console.log({tags,cats,maplinks});
+    if (!(await fs.exists(path.resolve(__dirname,'../output')))) {
+      await fs.mkdir(path.resolve(__dirname,'../output'));
+    }
+    await fs.writeFile(path.resolve('../output/legacy-post-paths.json'), JSON.stringify(maplinks, null, 2), 'utf8');
+    
+  } catch(err) {
+    console.error(err.message, err.stack.replace(/\\n/,'\n'));
+  }
 }
+
+function unique(arr) {
+  return arr.filter((o,i) => arr.lastIndexOf(o) == i);
+} 
 
 main();
